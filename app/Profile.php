771ed922cf526e418ebdaeaa76a0e3cc6564cfca
gpevalerio <pepedroid26@gmail.com>
2021-02-02 13:34:28 -0600

    se crean modelos y relaciones de los mismos

---
+++ app/Profile.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @author Valerio
+ * 29/01/2021
+ */
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Profile extends Model
+{
+    // Campos de la tabla que pueden ser alterados
+    protected $fillable = ["first_name","second_name","last_name","img","genre","born_date"];
+
+/**
+ * Relationship one to one
+ */
+ public function user(){
+   return $this->hasOne(User::class);
+ }
+}// class
