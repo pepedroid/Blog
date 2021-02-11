0de8133fe6495eeb25c1edbcabfdb51996758e40
gpevalerio <pepedroid26@gmail.com>
2021-02-10 19:14:00 -0600

    se importa el template para empezar el diseño de la aplicación y se generan las vistas publicas

---
+++ resources/views/main.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.appPublic')
+
+@section('js')
+    @extends('public.js')
+@endsection
+
+@section('footer')
+    @extends('public.footer')
+@endsection
+
+@section('content')
+    @extends('public.content')
+@endsection
+
+
+@section('navigation')
+    @extends('public.nav')
+@endsection
+
+@section('head')
+    @extends('public.head')
+@endsection
+
+
+
+
+
+@section('header')
+    @extends('public.header')
+@endsection
+
+
+@section('title',"Blog")
+
+
+
+
+
+
---
+++ resources/views/public/header.blade.php
@@ -0,0 +1,14 @@
+<!-- Page Header -->
+<header class="masthead" style="background-image: url('img/home-bg.jpg') ">
+    <div class="overlay"></div>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 col-md-10 mx-auto">
+                <div class="site-heading">
+                    <h1>Blog</h1>
+                    <span class="subheading">Un espacio para expresar tus ideas</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
