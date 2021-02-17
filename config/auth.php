7843117b5c12b7dc1342385d8397aee3c8d6b820
gpevalerio <pepedroid26@gmail.com>
2021-02-17 15:04:42 -0600

    se instala passport como método de autenticación

--- config/auth.php
+++ config/auth.php
@@ -42,7 +42,7 @@ return [
         ],
 
         'api' => [
-            'driver' => 'token',
+            'driver' => 'passport', //PASSPORT
             'provider' => 'users',
             'hash' => false,
         ],
